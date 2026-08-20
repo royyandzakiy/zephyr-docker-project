6b66ebee153a258d706e44d3aff6fb38fabd0832
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-08-20 14:57:13 +0700

    refactor: remove dead code

--- src/biz_logic.h
+++
@@ -1,5 +0,0 @@
-// #pragma once
-
-// void biz_logic_init(void);
-// void biz_logic_process(void);
-// int get_current_blink_interval(void);
--- src/main.c
+++ src/main.c
@@ -1,20 +1,3 @@
-// src/main.c
-// #include <zephyr/kernel.h>
-// #include "biz_logic.h"
-
-// int count = 0;
-
-// int main(void) {
-//     biz_logic_init();
-
-//     while (1) {
-//         biz_logic_process();
-//         printk("Hello from Zephyr RTOS! count = %d\n", ++count);
-//         k_msleep(get_current_blink_interval());
-//     }
-//     return 0;
-// }
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
